5038f61d7302b6e683ad5cf834a29c463ea94c0c
EUNJIN6131 <hailey6131@gmail.com>
2023-04-28 17:48:34 +0900

    수정

--- src/05/Frcst.js
+++ src/05/Frcst.js
@@ -1,17 +1,71 @@
+import { useState } from 'react';
 import data from './dataFrcst.json';
+import style from './Frcst.module.css';
+//state변수 1단계
 
-
-const Frcst  = () => {
-    console.log(data)
-    const dtKey = ["frcstOneDt","frcstTwoDt", "frcstThreeDt","frcstFourDt"]
+const Frcst = () => {
+    // console.log(data)map
+    const dtKey = ["frcstOneDt", "frcstTwoDt", "frcstThreeDt", "frcstFourDt"]
+    const cnKey = ["frcstOneCn", "frcstTwoCn", "frcstThreeCn", "frcstFourCn"]
     // map 배열
-    dtKey.map((item) => console.log(data[item]))
+    // dtKey.map((item) => console.log('dt',data[item]))
+    // cnKey.map((item) => console.log('cn',data[item]))
+
+
+    // object 만들기
+    let dtcn = {};
+    //dtkey배열만들기
+    dtKey.map((item, idx) => dtcn[data[item]] = data[cnKey[idx]]);
+    // console.log(dtcn)
+
+    // let tempTag;
+    // state 변수 2단계 : 변수선언
+    const [bodyTag, setBodyTag] = useState();
+    const [selDt, setselDt] = useState();
+
+    const detail = (k) => {
+        let dtcnItem = dtcn[k].split(',')
+        setselDt(k);
+        dtcnItem = dtcnItem.map((item) => item.split(':'));
+        dtcnItem = dtcnItem.map((item) =>
+            <div key={item[0]}>
+
+                <span className={style.sp1}>{item[0]}</span>
+                {item[1].trim() === '낮음' ? <span className={style.sp21}>{item[1]}</span>
+                    : item[1].trim() === '보통' ? <span className={style.sp22}>{item[1]}</span>
+                        : <span className={style.sp23}>{item[1]}</span>}
+            </div>
+        )
+        console.log(dtcnItem);
+        // console.log(k, dtcn[k]);
+        //state 변수 3단계 : 값 변경
+        setBodyTag(dtcnItem);
+    }
+
+    let dtTag = [];
+    dtTag = Object.keys(dtcn).map((item, idx) =>
+        <div className={selDt === item ? style.dt1 : style.dt}
+            key={idx}
+            onClick={() => detail(item)}>
+            {item}
+        </div>);
 
     return (
         <>
-       
+            <main className='container'>
+                <article>
+                    <header>
+                        <h1>초미세먼지 주간 예보</h1>
+                        <div className='grid'>
+                            {dtTag}
+                        </div>
+                    </header>
+                    <div className='grid'>{bodyTag}</div>
 
-    
+                </article>
+            </main>
+
+
 
         </>
     );
