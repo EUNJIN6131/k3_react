fc7cae6de1d52817d059e1694d40141340d449fd
EUNJIN6131 <hailey6131@gmail.com>
2023-05-04 13:07:30 +0900

    t

--- src/07/MyRef.js
+++ src/07/MyRef.js
@@ -1,23 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+// useEffect는 조건부함수 
 const MyRef = () => {
 
-    let cnt1 =1;
+    const txtref = useRef();
 
-    const showCnt = () => {
-        console.log('cnt1=', cnt1)
+    const itemArr = useRef([]);
+    const [itemTag, setItemTag] = useState();
+
+    useEffect(() => {
+        txtref.current.focus();
+    }, []);
+
+
+    const addItem = (e) => {
+        e.preventDefault();
+        itemArr.current = [...itemArr.current, txtref.current.value];
+        console.log("addItem", itemArr.current)
+    }
+
+    
+    const resetItem = () => {
+        console.log("resetItem")
     }
 
 
-    const showCnt1 = () => {
-        showCnt1();
-    }
     return (
         <main className="container">
             <article>
                 <header>
-                    <div className="grid">
-                        <button onClick={() => showCnt1()}>컴포넌트 변수 : {cnt1}</button>
-                    </div>
+                    <form>
+                        <div className="grid">
+                            <div>
+                                <label htmlFor="txt1">과일/채소 입력</label>
+                                <input ref={txtref} type="text" id="txt1" name="txt1" required />
+                            </div>
+
+                            <div><button onClick={(e) => addItem(e)}>등록</button>
+                                <button onClick={(e) => resetItem(e)}>취소</button></div>
+                        </div>
+                    </form>
                 </header>
+                    <div>
+                        {itemTag}
+                    </div>
             </article>
         </main>
 
