4df560b27679ef34dcd2be9aad38bcce2e4cd2d4
EUNJIN6131 <hailey6131@gmail.com>
2023-05-23 15:45:41 +0900

    dd

--- src/10/FcstTable.js
+++ src/10/FcstTable.js
@@ -1,4 +1,19 @@
-const FcstTable = () => {
+import {useState, useRef, useEffect} from 'react';
+
+const FcstTable = ({items, gubun}) => {
+
+    const [data, setData] =useState('');
+    
+    useEffect(()=> {},
+
+    [data])
+
+    let trTags = [];
+   // for (let row of mv) {
+       // console.log(row.rank, row.movieNm, row.salesAmt, row.rankInten);
+    
+
+
 
     return (
         <table>
@@ -10,7 +25,11 @@ const FcstTable = () => {
                     <th scope="col">예보 값</th>
                 </tr>
             </thead>
+            <tbody>
+            {data}
+            </tbody>
         </table>
+
     );
 }
 
--- src/10/VilageFcst.js
+++ src/10/VilageFcst.js
@@ -1,13 +1,57 @@
 import FcstTable from './FcstTable';
 import { useParams} from "react-router-dom";
+import code from './getcode.json';
+import {useState, useRef, useEffect} from 'react';
 
 const VilageFcst =() => {
 
+    const area = useParams().area;
+    const dt = useParams().dt;
+    const x = useParams().x;
+    const y = useParams().y;
 
+    const [items, setItems] = useState();
+        
+        useEffect(() => {
+            
+            let apikey = 'YbCtbs4SETsco3SXnbTi7otpks1RagK%2FQMCp5pQNZCbTxKz6Kewoc%2FQRvLrGDPjMoZPCe9ml2sJWVQsi01LmcA%3D%3D'
+            let url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?'
+            url = url + `serviceKey=${apikey}`;
+            url = url + `&numOfRows=60`;
+            url = url + `&pageNo=1`;
+            url = url + `&base_date=${dt}`;
+            url = url + `&base_time=0500`;
+            url = url + `&nx=55&ny=127`;
+            url = url + `&dataType=json`;
+    
+            
+            fetch(url)
+            .then((resp) => resp.json())
+            .then((data) => setItems(data.response.body.items))
+            .catch((err) => console.log(err))
+            
+            console.log(url)
+        
+    }, [items]);
+
+    
+
+    const ops = code.map((item) =>
+    <option value={item["예보구분"]} key={item["예보구분"]} >
+        {item["항목명"]}
+    </option>
+    ).filter((item) => item.props.value === "단기예보" );
+   
 
     return (
         <article>
-            <header>VilageFcst</header>
+            <div className='grid'>
+            <header>{area + " "}단기예보</header> 
+                <select id='sel' name='sel' >
+                <option value=''>선택</option>
+                {ops}
+                </select>
+                </div>
             <FcstTable />
         </article>
 
